180109e98f3ff713268915969b47cfb428118d78
Rcleend <rcleende@gmail.com>
2022-01-23 15:22:40 +0100

    Continued working on Fibrosis Dataset

--- datasets/fibrosis.py
+++ datasets/fibrosis.py
@@ -38,7 +38,7 @@ class FibrosisDataset(Dataset):
                 images.append(img)
 
         images = np.array(images)
-        print(len(images[3]))
+        print(len(images[4]))
 
         # Remove rgb dimension
         return images[:,:,:,0]
