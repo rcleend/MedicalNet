eb0085454b0b4df1c1141af2c5b1daed7b917473
Rcleend <rcleende@gmail.com>
2022-01-23 13:02:43 +0100

    Continued working on Fibrosis Dataset

--- datasets/fibrosis.py
+++ datasets/fibrosis.py
@@ -11,14 +11,14 @@ import numpy as np
 from torch.utils.data import Dataset
 import nibabel
 from scipy import ndimage
-import pandas as pd
 
 class FibrosisDataset(Dataset):
 
     def __init__(self, root_dir, img_list, sets):
-        self.data = pd.read_csv(root_dir + img_list)
-        print("Processing {} rows".format(len(self.data) + 1))
+        with open(root_dir + img_list, 'r') as f:
+            self.img_list = [line.strip() for line in f]
 
+        print("Processing {} datas".format(len(self.img_list)))
         self.root_dir = root_dir
         self.input_D = sets.input_D
         self.input_H = sets.input_H
@@ -27,16 +27,13 @@ class FibrosisDataset(Dataset):
 
 
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.img_list)
 
     def __getitem__(self, i):
-        df = self.data[i]
-        print('get item:')
-        print(df)
 
-        # TODO: Get corresponding ct scan images and change them to tensor values
-        # TODO: Get FVC value as y 
-        # TODO: Get 
+        # if self.phase == 'train':
+        #     print(self.img_list[i])
+        # elif self.phase == 'test':
+        #     print('test')
 
-
-        return i
+        return self.img_list[i]
