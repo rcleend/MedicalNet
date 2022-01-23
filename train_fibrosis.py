8e4cb280e3afa8a0a791af38489153eb9b4118b2
Rcleend <rcleende@gmail.com>
2022-01-23 21:34:31 +0100

    Implemented training code and added connection to dense layer

--- train_fibrosis.py
+++ train_fibrosis.py
@@ -44,7 +44,7 @@ def train(data_loader, model, optimizer, scheduler, total_epochs, save_interval,
             batch_id_sp = epoch * batches_per_epoch
             optimizer.zero_grad()
 
-            y_pred = model(x_batch)
+            y_pred = model(x_batch[0])
 
             # Calculate loss using mean squared error
             loss = mse(y_pred, y_batch)
@@ -115,4 +115,4 @@ if __name__ == '__main__':
     data_loader = DataLoader(training_dataset, batch_size=sets.batch_size, shuffle=True, num_workers=sets.num_workers, pin_memory=sets.pin_memory)
 
     # training
-    # train(data_loader, model, optimizer, scheduler, total_epochs=sets.n_epochs, save_interval=sets.save_intervals, save_folder=sets.save_folder, sets=sets) 
+    train(data_loader, model, optimizer, scheduler, total_epochs=sets.n_epochs, save_interval=sets.save_intervals, save_folder=sets.save_folder, sets=sets) 
