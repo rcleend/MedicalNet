87d398dc65edbe95f1124ae4a497bf5eb6b6f4fc
Rcleend <rcleende@gmail.com>
2022-01-30 09:50:47 +0100

    .

--- models/fibrosis.py
+++ models/fibrosis.py
@@ -60,7 +60,7 @@ class CustomDenseLayer(nn.Module):
       x = self.flatten(x)
     #   x = self.linear(x)
       #note: we clone the x tensors to prevent modification before computing the gradient
-      x = self.relu(x[:,0].clone()) #FVC value
+      x = self.relu(x.clone()) #FVC value
       x = self.linear(x)
     #   x[:,0] = self.relu(x[:,0].clone()) #FVC value
     #   x[:,1] = self.relu(x[:,1].clone()) ## Age
