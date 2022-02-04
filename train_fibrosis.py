5f1cb8964787c9305ce70b479dcf20aa8c54cd31
Rcleend <rcleende@gmail.com>
2022-02-04 13:50:33 +0100

    Updated loss function

--- train_fibrosis.py
+++ train_fibrosis.py
@@ -66,7 +66,7 @@ def train(data_loader, test_loader, model, optimizer, scheduler, total_epochs, s
             # Calculate loss using mean squared error
             loss = custom_loss(y_pred.to(torch.float32), y_batch.to(torch.float32)) / sets.batch_size
 
-            update_accuracy(train_acc, y_pred, y_batch)
+            update_accuracy(train_acc, y_pred, y_batch, sets)
 
             writer.add_scalar("Loss/train", loss, idx)
             writer.add_scalar("Accuracy_FVC/train", train_acc['fvc']/sets.batch_size, idx)
