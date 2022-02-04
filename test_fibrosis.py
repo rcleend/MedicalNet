4c537ce0e05ae8234cc76f3ddfb19886fcd3bd68
Rcleend <rcleende@gmail.com>
2022-02-04 11:26:12 +0100

    Started working on accuracy eval

--- test_fibrosis.py
+++ test_fibrosis.py
@@ -13,34 +13,40 @@ from torch.utils.data import DataLoader
 from utils.logger import log
 import pandas as pd
 import numpy as np
+import torch.nn as nn
 
 from torchsummary import summary
 from torch.utils.tensorboard import SummaryWriter
 
+mse = nn.MSELoss()
+bce = nn.BCELoss()
 
-def test(data_loader, model, confidence_iter, sets):
-    confidence_dict = {}
-    eval_df = pd.DataFrame(columns = ['Patient_Week', 'FVC', 'Confidence'])
+def test(data_loader, model, accuracy, sets):
+    for i, (x, y) in enumerate(data_loader):
+        y_pred = model(y)
 
-    # Create confidence probability distribution for all test data
-    for i in range(confidence_iter):
-        for j, (x, y, patient_id) in enumerate(data_loader):
-            y_pred = model(y)
+def update_accuracy(accuracy, y_pred, y):
+    loss = nn.BCELoss()
+    # get RMSE for FVC
+    fvc_rmse = rmse(y_pred[:,0], y[:,0])
+    print('fvc: ',fvc_rmse)
+    # accuracy['fvc'] += fvc_rmse
+    # get RMSE for Age
+    age_rmse = rmse(y_pred[:,1], y[:,1])
+    print('age: ',age_rmse)
+    # accuracy['age'] += age_rmse
+    # get accuracy for sex
+    sex_acc = bce(y_pred[:,2],y[:,2])
+    print('sex: ',sex_acc)
+    # accuracy['sex'].append(
+    # get accuracy for smoking
+    smok_acc = bce(y_pred[:,3:6],y[:,3:6])
+    print('smk: ', smok_acc)
+    # accuracy['smoking'].append()
 
-            # ID needs to exist out of patient id and week number
-            patient_week = patient_id + x[1]
-            fvc = y_pred.data[0]
+def rmse(pred, target):
+    return torch.sqrt(mse(torch.log(pred + 1), torch.log(target + 1)))
 
-            # Update confidence
-            confidence_dict[patient_week].append(fvc)
-
-            # On the last confidence iteration get confidence and add to eval_df
-            if i == confidence_iter - 1:
-                conf = np.std(confidence_dict[patient_week])
-                eval_df.append([patient_week, fvc, conf])
-
-    # TODO update name
-    eval_df.to_csv('eval_test.csv')
         
 if __name__ == '__main__':
     # settting
@@ -54,4 +60,5 @@ if __name__ == '__main__':
     test_dataset = FibrosisDataset(sets.data_root, 'test.csv', sets)
     data_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=False)
 
-    test(data_loader, model, confidence_iter=3, sets=sets) 
+    accuracy = {}
+    test(data_loader, model, accuracy, sets=sets) 
