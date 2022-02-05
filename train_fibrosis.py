f990ab202073c97aad07f7a23b8e0f9664314748
Rcleend <rcleende@gmail.com>
2022-02-05 09:26:49 +0100

    Removed prints

--- train_fibrosis.py
+++ train_fibrosis.py
@@ -144,10 +144,10 @@ def update_acc(acc, y_pred, y, sets):
 def smk_acc(y_pred, y):
     true = torch.argmax(y, dim=1) 
     pred = torch.argmax(y_pred, dim=1) 
-    print('smk y_pred',y_pred)
-    print('smk y', y)
-    print('smk pred',pred)
-    print('smk true',true)
+    # print('smk y_pred',y_pred)
+    # print('smk y', y)
+    # print('smk pred',pred)
+    # print('smk true',true)
     return true.cpu().detach().numpy().tolist(), pred.cpu().detach().numpy().tolist()
 
 
